af03fda7f7cab980168005cbd8866716f0b58bb4
Brandon <Brandonstowe71@gmail.com>
2021-04-21 14:32:09 -0500

    added in the pokemon variable to the array

--- js/script.js
+++ js/script.js
@@ -1,8 +1,22 @@
- let pokemonList = [];
+ let pokemonList = [pokemon1, pokemon2, pokemon3];
 
  let pokemon1 =
  {
-   name = "Pikachu";
-   height = 0.4;
-   type = ["electric"];
+   name: "Pikachu";
+   height: 0.4;
+   type: ["Electric"];
+ }
+
+ let pokemon2 =
+ {
+   name: "Charmander";
+   height: 0.6;
+   type: ["Fire"];
+ }
+
+ let pokemon3 =
+ {
+   name: "Squirtle";
+   height: 0.5;
+   type: ["Water"];
  }

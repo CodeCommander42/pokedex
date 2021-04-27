0a5a404325d9a24844a1cf1ab71354dac1f84e91
Brandon <Brandonstowe71@gmail.com>
2021-04-27 13:57:54 -0500

    Fixed PokemonList array

--- js/script.js
+++ js/script.js
@@ -1,27 +1,23 @@
- let pokemonList = [pokemon1, pokemon2, pokemon3];
-
- let pokemon1 =
- {
-   name: "Pikachu",
-   height: 0.4,
-   type: ["Electric"]
- };
-
- let pokemon2 =
- {
-   name: "Charmander",
-   height: 0.6,
-   type: ["Fire"]
- };
-
- let pokemon3 =
- {
-   name: "Squirtle",
-   height: 0.5,
-   type: ["Water"]
- };
+ let pokemonList = 
+ [
+    {
+      name: "Pikachu",
+      height: 0.4,
+      type: ["Electric"]
+    },
+    {
+      name: "Charmander",
+      height: 0.6,
+      type: ["Fire"]
+    },
+    {
+      name: "Squirtle",
+      height: 0.5,
+      type: ["Water"]
+    }
+ ]
 
  for (let i = 0; i < pokemonList.length; i++)
   {
-    document.write(pokemonList[i].name + "(" +  pokemonList[i].height) + ")";
+    document.write(pokemonList[i].name + "(" + "Height " + pokemonList[i].height + ") ");
   }

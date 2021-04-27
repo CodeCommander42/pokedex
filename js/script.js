f54b881c33ed733fbb357b9359473c0aead69790
Brandon <Brandonstowe71@gmail.com>
2021-04-27 13:47:58 -0500

    trying to get the list printed

--- js/script.js
+++ js/script.js
@@ -5,24 +5,23 @@
    name: "Pikachu",
    height: 0.4,
    type: ["Electric"]
- }
+ };
 
  let pokemon2 =
  {
    name: "Charmander",
    height: 0.6,
    type: ["Fire"]
- }
+ };
 
  let pokemon3 =
  {
    name: "Squirtle",
    height: 0.5,
    type: ["Water"]
- }
+ };
 
  for (let i = 0; i < pokemonList.length; i++)
-{
-  document.write(pokemonList[i].name + pokemonList[i].height);
-}
-
+  {
+    document.write(pokemonList[i].name + "(" +  pokemonList[i].height) + ")";
+  }

f75a2d6f719e805f1f814fe7f2b5d8375906135a
Brandon <Brandonstowe71@gmail.com>
2021-05-05 12:59:20 -0500

    updated main branch to a list that works

--- js/script.js
+++ js/script.js
@@ -1,41 +1,32 @@
+let pokemonList = 
+[
+   {
+     name: "Pikachu",
+     height: 0.4,
+     type: ["Electric"]
+   },
+   {
+     name: "Charmander",
+     height: 0.6,
+     type: ["Fire"]
+   },
+   {
+     name: "Squirtle",
+     height: 0.5,
+     type: ["Water"]
+   }
+]
 
-  let pokemonRepository = (function () {
-    let pokemonList = 
-    [
-       {
-         name: "Pikachu",
-         height: 0.4,
-         type: ["Electric"]
-       },
-       {
-         name: "Charmander",
-         height: 0.6,
-         type: ["Fire"]
-       },
-       {
-         name: "Squirtle",
-         height: 0.5,
-         type: ["Water"]
-       }
-    ]
-
-    function add(pokemon) {
-      pokemonList.push(pokemon);
-    }
-
-    function getAll() {
-      return pokemonList;
-    }
-
-    return {
-      add: add,
-      getAll: getAll
-    };
-  })();
-
- pokemonRepository.getAll().forEach(function (pokemon) {
-  document.write(pokemonRepository.getAll());
- })
- 
+for (let i = 0; i < pokemonList.length; i++)
+ {
+   if (pokemonList[i].height > 0.5)
+   {
+     document.write(pokemonList[i].name + " (" + "Height " + pokemonList[i].height + ") " + "- Wow thats big! <br />");
+   }
+   else 
+   {
+     document.write(pokemonList[i].name + " (" + "Height " + pokemonList[i].height + ") <br />");
+   }
+ }
 
  
